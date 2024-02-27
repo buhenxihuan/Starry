82e902bb4e2b199c82fab8b59aff155c1fed04b2
buhenxihuan <1127751018@qq.com>
2024-02-27 20:44:53 +0800

    modified:   env.py

--- env.py
+++ env.py
@@ -1,7 +1,11 @@
 import os
-print(os.environ)
-print(os.environ.get("ARCH"))
-env=os.getenv('GITHUB_ENV')
-print(env)
-arch=os.getenv('ARCH')
-print(arch)
+from os import environ as env
+
+file_path = os.environ.get('GITHUB_ENV', None)
+if file_path is None:
+    raise OSError('Environment file not found')
+
+# 读取文件内容并打印
+with open(file_path, 'r') as gh_envs:
+    for line in gh_envs:
+        print(line.strip())  # 打印每一行内容（移除开头和结尾的空白符）
