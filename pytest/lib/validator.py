5fe4ba52223b78ec6acc688647b990ebedebda35
buhenxihuan <1127751018@qq.com>
2024-03-05 17:50:29 +0800

    modified:   pytest/lib/validator.py

--- pytest/lib/validator.py
+++ pytest/lib/validator.py
@@ -21,7 +21,7 @@ class validator:
         m1 = re.findall('( fault | error |^fail |^fail!|Segmentation fault)', caseRes, re.IGNORECASE|re.MULTILINE)
         m2 = re.findall('(pass|success)', caseRes, re.IGNORECASE|re.MULTILINE)
         m3 = re.findall('(.*not found|.*unknown operand)', caseRes, re.IGNORECASE|re.MULTILINE)
-        print(m3)
+        # print(m3)
         if m1:
             succCaseNum = len(m2) if m2 else 0
             return False, "测试失败，失败用例条数：%s，成功用例条数：%s，错误信息：\n%s" %(len(m1), succCaseNum, '\n'.join(m3))
