6d06990f9db54b077429ed322a4d6c6ee111f9db
buhenxihuan <32200913+buhenxihuan@users.noreply.github.com>
2024-03-08 15:25:43 +0800

    modified:   pytest/lib/cmd.py 	modified:   pytest/lib/validator.py 	modified:   pytest/testcase/test_arceos.py

--- pytest/lib/cmd.py
+++ pytest/lib/cmd.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python3
-# -*- coding:utf-8 -*-
+# #!/usr/bin/python3
+# # -*- coding:utf-8 -*-
 
-import os
-import sys
-import subprocess
-from lib.timeout import communicate_with_timeout, TimeoutExpired
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(BASE_DIR)
-sys.path.append(BASE_DIR)
+# import os
+# import sys
+# import subprocess
+# from lib.timeout import communicate_with_timeout, TimeoutExpired
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# print(BASE_DIR)
+# sys.path.append(BASE_DIR)
 
 
 # class cmd:
@@ -43,29 +43,75 @@ sys.path.append(BASE_DIR)
 #     else:
 #         print('命令执行失败, code = %s, out = %s'  %(code, out))
 #         sys.exit(1)
+
+
+
+
+
+# class cmd:
+#     def __init__(self):
+#         self.test = 'test'
+
+#     # 执行非交互命令
+#     def run_cmd(self, cmd, timeout=None):
+#         p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        
+#         try:
+#             stdout, stderr = communicate_with_timeout(p, timeout=100)
+#             result = self.decode_output(stdout)
+#         except TimeoutExpired:
+#             result = "Command execution timed out"
+        
+#         return p.returncode, result
+
+#     def decode_output(self, output):
+#         try:
+#             result = output.decode('utf-8').strip()
+#         except UnicodeError:
+#             try:
+#                 result = output.decode('gbk').strip()
+#             except UnicodeError:
+#                 result = output.decode('ansi').strip()
+
+#         return result
+
+
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+import os
+import sys
+import subprocess
+import signal
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR)
+sys.path.append(BASE_DIR)
+
 class cmd:
     def __init__(self):
         self.test = 'test'
 
     # 执行非交互命令
-    def run_cmd(self, cmd, timeout=None):
-        p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        
+    def run_cmd(self, cmd):
+        p = None
         try:
-            stdout, stderr = communicate_with_timeout(p, timeout=100)
-            result = self.decode_output(stdout)
-        except TimeoutExpired:
-            result = "Command execution timed out"
-        
-        return p.returncode, result
+            p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            stdout, stderr = p.communicate(timeout=200)
+        except Exception as e:
+            if p:
+                os.killpg(p.pid, signal.SIGKILL)
+            return -1, "测试发生异常: %s" %str(e)
 
-    def decode_output(self, output):
+        result = ''
+
+        # 获取输出时注意编码错误
         try:
-            result = output.decode('utf-8').strip()
+            result = stdout.decode('utf8').strip()
         except UnicodeError:
             try:
-                result = output.decode('gbk').strip()
+                result = stdout.decode('gbk').strip()
             except UnicodeError:
-                result = output.decode('ansi').strip()
+                result = stdout.decode('ansi').strip()
 
-        return result
+        return p.returncode, result
--- pytest/lib/validator.py
+++ pytest/lib/validator.py
@@ -4,6 +4,7 @@
 import os
 import sys
 import re
+from unittest import case
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print(BASE_DIR)
@@ -16,12 +17,12 @@ class validator:
 
     def check(self, retCode, caseRes):
         if retCode != 0:
-            return False, "测试失败，命令执行失败"
+            return False, "测试失败，命令执行失败，错误信息：\n%s" %caseRes
 
         m1 = re.findall('( fault | error |^fail |^fail!|Segmentation fault)', caseRes, re.IGNORECASE|re.MULTILINE)
         m2 = re.findall('(pass|success)', caseRes, re.IGNORECASE|re.MULTILINE)
         m3 = re.findall('(.*not found|.*unknown operand)', caseRes, re.IGNORECASE|re.MULTILINE)
-        # print(m3)
+        print(m3)
         if m1:
             succCaseNum = len(m2) if m2 else 0
             return False, "测试失败，失败用例条数：%s，成功用例条数：%s，错误信息：\n%s" %(len(m1), succCaseNum, '\n'.join(m3))
